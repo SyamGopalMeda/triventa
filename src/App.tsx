5b3e55093d9cdb8a1eb3ba6f2ba550c27fd639fb
SyamGopalMeda <107456943+SyamGopalMeda@users.noreply.github.com>
2025-08-10 14:59:41 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { HeroSection } from './components/HeroSection';
 import { AboutSection } from './components/AboutSection';
 import { BusinessVerticals } from './components/BusinessVerticals';
-import { EcommerceSection } from './components/EcommerceSection';  // Add this import
+import { EcommerceSection } from './components/EcommerceSection';
+import { ManagementSection } from './components/ManagementSection';  // Add this import
 import { LocationsSection } from './components/LocationsSection';
 import { ContactSection } from './components/ContactSection';
 import { Footer } from './components/Footer';
@@ -13,7 +14,8 @@ function App() {
       <HeroSection />
       <AboutSection />
       <BusinessVerticals />
-      <EcommerceSection />  {/* Add this component */}
+      <EcommerceSection />
+      <ManagementSection />  {/* Add this component - positioned after business verticals for logical flow */}
       <LocationsSection />
       <ContactSection />
       <Footer />
